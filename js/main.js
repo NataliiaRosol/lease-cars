4f21247dbed5161536dea672a89d7e078c216f63
NataliiaRosol <93214918+NataliiaRosol@users.noreply.github.com>
2024-08-23 12:57:43 +0300

    finished layout  for desctop

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+const tabItem = document.querySelectorAll('.tabs-btn-item');
+const tabContent = document.querySelectorAll('.tabs-content-item');
+
+tabItem.forEach((elem) => {
+  elem.addEventListener('click', open)
+});
+
+function open(event){
+  const tabTarget = event.currentTarget;
+  const button = tabTarget.dataset.button;
+  
+  //tab buttons
+  tabItem.forEach((item)=> {
+    item.classList.remove('tabs-btn-item-active')
+  });
+  tabTarget.classList.add('tabs-btn-item-active');
+
+  //tab content
+  tabContent.forEach((item)=>{
+    item.classList.remove('tabs-btn-item-active');
+  });
+
+  document.querySelector(`#${button}`).classList.add('tabs-btn-item-active')
+}
+
+
+
 const swiper = new Swiper(".swiper", {
   effect: "fade",
   pagination: {
